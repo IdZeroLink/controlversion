cf4bd2c4b1b9a5a0b7856f512feca43d9fe5aeb7
IdZeroLink <valertosboxer@yandex.ru>
2023-06-18 19:47:58 +0300

    Add readme and main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        long[] sales = new long[]{1, 2, 300, 200};
+
+        SalesManager salesManager = new SalesManager(sales);
+        long max = salesManager.max();
+        long min = salesManager.min();
+        long average = salesManager.average();
+
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
+    }
+}

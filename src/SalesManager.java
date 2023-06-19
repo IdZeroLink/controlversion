973f0caea8df7db9451508ad626da063e327f07f
IdZeroLink <136549150+IdZeroLink@users.noreply.github.com>
2023-06-19 21:49:16 +0300

    Update SalesManager.java

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,35 +1,17 @@
 public class SalesManager {
-    protected long [] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
     public long max() {
-        long max = -1;
-        for (long sale : sales) {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
-    }
-
-    public long min() {
-        long min = Integer.MAX_VALUE;
-        for (long sale : sales) {
-            if (min > sale) {
-                min = sale;
-            }
-        }
-        return min;
-    }
-
-    public long average() {
-        long sum = 0;
-        for (long sale : sales) {
-            sum += sale;
-        }
-        return (sum - (min() + max())) / (sales.length - 2);
     }
 }

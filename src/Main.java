9791c3d858f936655af84cdc30234491d37f4f82
IdZeroLink <136549150+IdZeroLink@users.noreply.github.com>
2023-06-19 21:50:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        long[] sales = new long[]{1, 2, 300, 200};
+        int[] sales = new int[]{1, 2, 300, 200};
 
         SalesManager salesManager = new SalesManager(sales);
-        long max = salesManager.max();
-        long min = salesManager.min();
-        long average = salesManager.average();
+        int max = salesManager.max();
+     
 
         System.out.println(max);
-        System.out.println(min);
-        System.out.println(average);
     }
 }
